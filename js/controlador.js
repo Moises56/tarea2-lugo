e65fbc2fcd50b910aebe2b4cb726b78cc1536609
Moises56 <moiavil56@gmail.com>
2021-06-07 18:54:15 -0600

    second commit

--- js/controlador.js
+++ js/controlador.js
@@ -497,6 +497,8 @@ var categorias = [
 
 
 localStorage = window.localStorage
+indiceSelect = null;
+proSelect = null;
 
 if (localStorage.getItem("categorias") == null) {
     localStorage.setItem("categorias", JSON.stringify(categorias));
@@ -527,16 +529,12 @@ function cambiarUsuario(){
     document.getElementById('user-actual').innerHTML = 
     `<h1 class="nombre-inicial">!Hola ${userActual}!</h1>`
 
-// document.getElementById('user-title').innerHTML =
-// `<h5 class="modal-title title-order" id="exampleModalLabel">${userActual}, Estas son tus Ordenes </h5>`
-
-
-
 }cambiarUsuario()
 
 
 //* Generar Categorias
 function geneCategortias(){
+    document.getElementById('categorias').innerHTML = ''; 
     categorias.forEach(function(app, i) {
 
         document.getElementById('categorias').innerHTML += 
@@ -550,7 +548,9 @@ function geneCategortias(){
             </div>
         </div>
         `
-    });
+       
+
+});
 
 
 }geneCategortias();
@@ -568,7 +568,7 @@ function userOrder(){
             order = user.ordenes
             document.getElementById('modalOrder').innerHTML = '';
             for (let i = 0; i < order.length; i++) {
-                console.log(order[i].nombreProducto);
+                // console.log(order[i].nombreProducto);
                 nombreProducto = order[i].nombreProducto
                 cantidad = order[i].cantidad
                 descripcion = order[i].descripcion
@@ -582,17 +582,205 @@ function userOrder(){
                         <span class="precio"><b>$${ precio}</b></span>
                         <hr>
                      </div>
-                `
-                
-            }
-
-            
+                ` 
+            }           
         }
         
     });
     // console.log('order');
 }
-// userActual = document.getElementById('usuario-actual').value
-// document.getElementById('user-title').innerHTML =
-// `<h5 class="modal-title title-order" id="exampleModalLabel">${userActual}, Estas son tus Ordenes </h5>`
+
+
+// var array_valores = Array();
+
+// function add(x)
+// {
+
+//     var input = document.getElementById('node');
+//     array_valores.push(x.innerHTML);
+
+//     input.value = array_valores.toString();                         
+
+// }
+   
+
+
+
+
+//* mostrar categorias
+function modalCategorias(i){
+    indiceSelect = i  
+    document.getElementById('modalCategorias').innerHTML ='';
+
+    nombreCategoria = categorias[i].nombreCategoria
+    document.getElementById('nomCate').innerHTML =
+    `<h5 class="modal-title title-order" id="categoriasModalLabel">${nombreCategoria}</h5>`
+
+
+    empresas = categorias[i].empresas   
+    empresas.forEach(function(app,p) {
+        // console.log(app.nombreEmpresa);
+        nameEmpresa = app.nombreEmpresa
+        imgEmpresa = app.imagen     
+
+       
+
+       
+        productos = app.productos
+        html ='';
+        for (let p = 0; p < productos.length; p++) {
+            
+            // console.log(productos[p].nombreProducto);
+            nombreProducto = productos[p].nombreProducto
+            descripcion = productos[p].descripcion
+            precio = productos[p].precio
+       
+            html+= ` <div class="card-body">
+                        <h3 id="nombre-prod" name="nom-Pro" class="nombre-Producto">${nombreProducto}</h3>
+                        <button onclick="prodSelect(${p})" data-bs-toggle="modal" data-bs-target="#procesarModal" type="button" class="btn btn-pedir">Pedir</button> 
+                        <p id="descripcion-prod" class="descripcion-prod">${descripcion}</p>
+                        <span id="precio-pro" class="precio2">$${precio}</span>
+                        <hr> <br>
+                    </div>`
+     
+        }
+        document.getElementById('modalCategorias').innerHTML +=
+        `<div class="col-6">
+            <div class="card" > 
+                <img src="${imgEmpresa}" class="card-img-top" alt="...">
+                <h5 class="card-title nomEmpresa">${nameEmpresa}</h5>        
+                ${html}            
+            </div>
+        </div> 
+        `
+        
+    });
+
+}
+
+//*Procesar Pedido
+
+function prodSelect(p){
+    proSelect = p
+    // var nombrePro = document.getElementById('nombre-prod').innerHTML;
+    var  descripcionPro = document.getElementById('descripcion-prod').innerHTML; 
+    var precioPro = document.getElementById('precio-pro').innerHTML;
+
+    var a = document.getElementsByTagName('h3')
+    console.log(a[p].innerHTML);
+
+    document.getElementById('nPro').innerHTML = `<h2 class="nombre-Producto2">${a[p].innerHTML}</h2>`
+    document.getElementById('desc').innerHTML = `<p class="descripcion-Producto2>${descripcionPro}</p>`
+    document.getElementById('prec').innerHTML = `<span class="precio3" id="prec">${precioPro}</span>`
+    
+    
+}
+
+//* guarduar una orden para cada usuario
+ function procesarOrden(){
+     i = indiceSelect //*categoria
+     o = proSelect //* # orden
+     console.log('procesando2...',i);
+
+     var a = document.getElementsByTagName('h3')
+    console.log(a[o].innerHTML);
+        
+ 
+      let nomUserActual = document.getElementById('usuario-actual').value 
+      if (nomUserActual == null) {
+          nomUserActual = 'Pedro Martinez'
+      }
+      let user = usuarios;
+       for (let i in user) {
+           order = user[i].ordenes
+           nome = user[i].nombre+ ' ' + user[i].apellido;
+        //    console.log(nome);
+          //*Compara nombre actual con el del arreglo
+           if (nomUserActual == nome) {
+        //    console.log(true);    
+           ordenes = usuarios
+           ordenes = {
+                       nombreProducto:a[o].innerHTML,
+                       descripcion:document.getElementById('descripcion-prod').innerHTML,
+                       cantidad:document.getElementById('cant').value,
+                       precio:document.getElementById('precio-pro').innerHTML
+                   };
+                   order.push(ordenes)
+               
+                 userOrder()
+                 localStorage.setItem("usuarios", JSON.stringify(usuarios));
+                 alert('Pedido con Exito')
+           }
+      }
+
+ }
+
+
+iconos = ['fab fa-atlassian', 'fas fa-atom', 'fab fa-apple']
+for (let i = 0; i < iconos.length; i++) {
+    document.getElementById('list-new').innerHTML +=
+    `<option value="${iconos[i]}">Icono${i}</option>`    
+}
+
+
+ //* guarduar una nueva categoria
+function newCategoria(){
+    console.log('new categoria');
+
+    nameNew = document.getElementById('name-new').value
+    descNew = document.getElementById('desc-new').value
+    colorNew = document.getElementById('color-new').value
+    listNew = document.getElementById('list-new').value
+    empreNew = document.getElementById('empre-new').value
+
+    console.log(nameNew);
+    console.log(descNew);
+    console.log(colorNew);
+    console.log(listNew);
+    console.log(empreNew);
+
+    category = {
+        nombreCategoria:nameNew,
+        descripcion:descNew,
+        color:colorNew,
+        icono:listNew,
+        empresas:[
+            {
+                nombreEmpresa: empreNew,
+                imagen:"img/banner.jpg",
+                productos:[
+                    {
+                        nombreProducto: "Producto N1",
+                        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto N2",
+                        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto N3",
+                        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                        precio: 49.99
+                    },
+                    {
+                        nombreProducto: "Producto N4",
+                        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, modi!",
+                        precio: 49.99
+                    }
+                ]
+            }
+        ]
+    }
+    console.log(category);
+    categorias.push(category)
+    geneCategortias();
+    localStorage.setItem("categorias", JSON.stringify(categorias));
+
+}
+
+
+
+
 
